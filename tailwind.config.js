2d92a7cdc149493e904815f73819812a8a5dee03
emmadumbi <dumbiemmanuel29@gmail.com>
2024-11-20 01:52:13 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        Soft_Blue: 'hsl(231, 69%, 60%)',
+        Soft_Red: 'hsl(0, 94%, 66%)',
+        Grayish_Blue: 'hsl(229, 8%, 60%)',
+        Very_Dark_Blue: 'hsl(229, 31%, 21%)'
+      },
+    },
+  },
+  plugins: [],
+}
+
