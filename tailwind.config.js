7a5ca5d1ba1fd0f1103ccb8a9067a24addf38d6c
emmadumbi <dumbiemmanuel29@gmail.com>
2024-11-20 11:53:39 +0100

    built the mobile nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ module.exports = {
         Grayish_Blue: 'hsl(229, 8%, 60%)',
         Very_Dark_Blue: 'hsl(229, 31%, 21%)'
       },
+      letterSpacing: {
+        wide: '0.2em',
+        wider: '0.26em',
+      },
     },
   },
   plugins: [],
